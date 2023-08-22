46b9f42ae9863e965bb286a723a7e7547f03c61b
holczur <holcz.norbert@gmail.com>
2023-08-22 19:07:14 +0200

    override loadUserByUsername()

--- src/main/java/com/example/springsecurity6demo/config/BankUserDetails.java
+++ src/main/java/com/example/springsecurity6demo/config/BankUserDetails.java
@@ -1,10 +1,17 @@
 package com.example.springsecurity6demo.config;
 
+import com.example.springsecurity6demo.domain.Customer;
 import com.example.springsecurity6demo.repository.CustomerRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BankUserDetails implements UserDetailsService {
@@ -17,6 +24,15 @@ public class BankUserDetails implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Customer customer = customerRepository.findByEmail(username).orElse(null);
+        if (customer == null) {
+            throw new UsernameNotFoundException("User details not found for the user : " + username);
+        } else {
+            String userName = customer.getEmail();
+            String password = customer.getPwd();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+            return new User(userName, password, authorities);
+        }
     }
 }
