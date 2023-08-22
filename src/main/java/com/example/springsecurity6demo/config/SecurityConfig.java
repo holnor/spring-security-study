9e767650b0277d95760f219828119f079da7f0b8
holczur <holcz.norbert@gmail.com>
2023-08-22 19:06:11 +0200

    change BCrypt to NoOp just to work with unhashed PW

--- src/main/java/com/example/springsecurity6demo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity6demo/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,6 +23,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
