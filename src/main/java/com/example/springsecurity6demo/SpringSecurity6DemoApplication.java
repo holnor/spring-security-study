937e5eb5f10d4421ffcd24d79f8435e171532e33
holczur <holcz.norbert@gmail.com>
2023-08-22 14:48:03 +0200

    Security Starter

---
+++ src/main/java/com/example/springsecurity6demo/SpringSecurity6DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity6DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurity6demo/SpringSecurity6DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
