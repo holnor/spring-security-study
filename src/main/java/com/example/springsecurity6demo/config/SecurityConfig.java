f9ed752b7141bb6b3c1474edda58fca60ec0371d
holczur <holcz.norbert@gmail.com>
2023-08-23 10:55:41 +0200

    auhenticate on "/user"

--- src/main/java/com/example/springsecurity6demo/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity6demo/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
                     }
                 })).csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((requests)->requests
-                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                        .requestMatchers("/notices", "/contact", "/register", "/user").permitAll())
+                        .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
