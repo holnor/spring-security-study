1b93b23bb3a7af829693b40b109b4572c494239e
holczur <holcz.norbert@gmail.com>
2023-08-23 08:39:00 +0200

    created

---
+++ src/main/java/com/example/springsecurity6demo/config/BankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.example.springsecurity6demo.config;
+
+import com.example.springsecurity6demo.domain.Customer;
+import com.example.springsecurity6demo.repository.CustomerRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public BankUsernamePwdAuthenticationProvider(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Customer customer = customerRepository.findByEmail(username).orElse(null);
+        if (customer != null) {
+            if (passwordEncoder.matches(pwd, customer.getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        //copied from DaoAuthProv.
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
