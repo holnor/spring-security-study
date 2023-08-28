77624a31acb3ae82bf4ed819d9ec85c7fc745138
holczur <holcz.norbert@gmail.com>
2023-08-28 10:23:40 +0200

    enable method security

--- src/main/java/com/example/springsecurity6demo/SpringSecurity6DemoApplication.java
+++ src/main/java/com/example/springsecurity6demo/SpringSecurity6DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.springsecurity6demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true,  securedEnabled = true,  jsr250Enabled = true)
 public class SpringSecurity6DemoApplication {
 
     public static void main(String[] args) {
